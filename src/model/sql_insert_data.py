b4bc5ec939c0db616e28c063ac781904027e765b
Jakub Chmiel <Jakub.Chmiel@fenergo.com>
2021-04-08 18:44:33 +0200

    check if example data already inserted

--- src/model/sql_insert_data.py
+++ src/model/sql_insert_data.py
@@ -4,5 +4,7 @@ from src.model.system.idea import Idea
 def sql_insert_data(db):
     raw = get('config/root_ideas.yml')
     for raw_idea in raw:
-        i = Idea(None, None, raw_idea['name'], raw_idea['content'])
-        i.save_to_db()
+        found = Idea.find_by_name(raw_idea['name'])
+        if (not found):
+            i = Idea(None, None, raw_idea['name'], raw_idea['content'])
+            i.save_to_db()
