b980f4f40be785325356a3c4090d5621dafba918
Jakub Chmiel <Jakub.Chmiel@fenergo.com>
2021-04-15 20:36:30 +0200

    initial data with parents

--- src/model/sql_insert_data.py
+++ src/model/sql_insert_data.py
@@ -3,8 +3,14 @@ from src.model.system.idea import Idea
 
 def sql_insert_data(db):
     raw = get('config/root_ideas.yml')
+    inserted = dict()
     for raw_idea in raw:
         found = Idea.find_by_name(raw_idea['name'])
         if (not found):
-            i = Idea(None, None, raw_idea['name'], raw_idea['content'])
+            parent_id = None
+            if ('parent' in raw_idea):
+                parent_id = inserted[raw_idea['parent']]
+            i = Idea(parent_id, None, raw_idea['name'], raw_idea['content'])
             i.save_to_db()
+            inserted[i.name] = i.id
+
