3e3235f0d5276b76d205751049d9f99597f8d990
Jakub Chmiel <Jakub.Chmiel@fenergo.com>
2021-12-04 17:30:08 +0100

    wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+abspath = os.path.abspath(__file__)
+path = os.path.join(os.path.dirname(abspath), 'src')
+os.chdir(path)
+sys.path.insert(1, path)
+
+from config.config import Config
+from model.db import DB, db
+
+DB(Config())
+
+from app import app
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
