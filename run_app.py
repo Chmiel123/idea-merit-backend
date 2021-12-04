89909bc63d7a5f5e6c0c5573e1be5679ec55711c
Jakub Chmiel <chmiel123@gmail.com>
2021-12-04 12:40:39 +0100

    fix os path.

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 abspath = os.path.abspath(__file__)
-path = os.path.dirname(abspath) + '\\src'
+path = os.path.join(os.path.dirname(abspath), 'src')
 os.chdir(path)
 sys.path.insert(1, path)
 
